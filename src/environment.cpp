ef1e37efd35a05b86a2bae58decbdedc609249ec
James Brodovsky <james.brodovsky@gmail.com>
2021-06-28 12:07:15 -0400

    Prior to 1D ransace implementation.

--- src/environment.cpp
+++ src/environment.cpp
@@ -45,9 +45,18 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     bool renderScene = true;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
-    // TODO:: Create lidar sensor 
+    // Create lidar sensor 
+    Lidar* lidar = new Lidar(cars, 0);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar->scan();
+    //renderRays(viewer, lidar->position, inputCloud);
+    //renderPointCloud(viewer, inputCloud, "InputCloud");
 
     // TODO:: Create point processor
+    ProcessPointClouds<pcl::PointXYZ> pointProcessor;
+    std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor.SegmentPlane(inputCloud, 100, 0.2);
+    //pointProcessor->SegmentPlane(inputCloud, 100, 0.2);
+    renderPointCloud(viewer, segmentCloud.first, "obstacles", Color(1,0,0));
+    renderPointCloud(viewer, segmentCloud.second, "Plane", Color(0,1,0));
   
 }
 
